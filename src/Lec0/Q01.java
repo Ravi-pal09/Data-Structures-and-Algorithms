f354c7999ed57fb37e5317fb66d1258f1ea68c69
Ravi-pal09 <rkp101719@gmail.com>
2026-07-15 22:00:48 +0530

    Add Lec0 java Program

---
+++ src/Lec0/Q01.java
@@ -0,0 +1,7 @@
+package Lec0;
+
+public class Q01 {
+    static void main(String[] args) {
+        System.out.println("Welocme to java");
+    }
+}
---
+++ src/Lec0/Qn02.java
@@ -0,0 +1,8 @@
+package Lec0;
+
+public class Qn02 {
+    static void main(String[] args) {
+        int x=21;
+        System.out.println(x);
+    }
+}
