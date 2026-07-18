cb67a8062ff500621b01c7ab2bf86b86e2a03dad
Ravi-pal09 <rkp101719@gmail.com>
2026-07-18 10:47:18 +0530

    Day02

---
+++ src/Level02/Qn02.java
@@ -0,0 +1,10 @@
+package Level02;
+//Print Numbers from N to 1
+public class Qn02 {
+    static void main(String[] args) {
+        int n=5;
+        for (int i=n; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
