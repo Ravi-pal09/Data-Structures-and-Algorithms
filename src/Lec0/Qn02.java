de4cdcda7b1c508f951ba15bb91d6473cf13aaba
Ravi-pal09 <rkp101719@gmail.com>
2026-08-23 21:37:59 +0530

    Restore Java project structure

--- src/Lec0/Qn02.java
+++
@@ -1,8 +0,0 @@
-package Lec0;
-
-public class Qn02 {
-    static void main(String[] args) {
-        int x=21;
-        System.out.println(x);
-    }
-}
--- src/Level01/Qn01.java
+++
@@ -1,7 +0,0 @@
-package Level01;
-//Print Hello, World!
-public class Qn01 {
-    static void main(String[] args) {
-        System.out.println("Print Hello, World!");
-    }
-}
